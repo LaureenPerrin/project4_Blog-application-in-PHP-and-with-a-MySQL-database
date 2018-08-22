dafc815ccfd85937ac245001c3038f879e6de5f5
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-22 17:00:41 +0200

    adding a comment entity v1

---
+++ structure_in_MVC/Model/Entity/Model_Entity_Comment.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace projet4\Blog\Model;
+
+class Comment
+{
+    private $_idComment;
+    private $_idEpisode;
+    private $_author;
+    private $_content;
+    private $_addDate;
+
+    public function __construct()
+    {
+        $this->hydrate();
+    }
+    
+    public function hydrate()
+    {
+    }
+    
+    //fonctions getters
+    public function getIdComment()
+    {
+        return $this->_idComment;
+    }
+
+    public function getIdEpisode()
+    {
+        return $this->_idEpisode;
+    }
+
+    public function getAuthor()
+    {
+        return $this->_author;
+    }
+
+    public function getContent()
+    {
+        return $this->_content;
+    }
+
+    public function getAddDate()
+    {
+        $date = new \DateTime($this->_addDate);
+        return $date->format('d-m-Y à H:i');
+    }
+    
+    //fonctions setters
+    public function setIdComment()
+    {
+        if (is_int($idComment) && $idComment >= 0) {
+            $this->_idComment = $idComment;
+        }
+        return $this;
+    }
+
+    public function setIdEpisode()
+    {
+        if (is_int($idEpisode) && $idEpisode >= 0) {
+            $this->_idEpisode = $idEpisode;
+        }
+        return $this;
+    }
+
+     public function setAuthor($author)
+    {
+        if (is_string($author) && strlen($author) <= 70) {
+            $this->_author = $author;
+        }
+        return $this;
+    }
+
+    public function setContent($content)
+    {
+        $content = htmlspecialchars_decode($content);
+        if (is_string($content)) {
+            $this->_content = $content;
+        }
+        return $this;
+    }
+
+    public function setAddDate($addDate)
+    {
+        $this->_addDate = $addDate;
+        return $this;
+    }
+}
