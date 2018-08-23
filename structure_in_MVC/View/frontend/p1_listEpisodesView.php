e5262e77fd1025d52c419aaad2cc054bf4572171
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-23 08:02:19 +0200

    adding first page of blog v1 (p1 frontend list episodes view)

---
+++ structure_in_MVC/View/frontend/p1_listEpisodesView.php
@@ -0,0 +1,39 @@
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+<img src="public/images/alaska2.jpg" alt="payasage alaska" />
+<h1>Bonjour et bienvenue chers lecteurs!</h1>
+<p>Grâce à mon blog vous pouvez découvrir, au fur et mesure de sa création, mon nouveau roman "Billet simple pour l'Alaska".
+    Je publierais régulièrement mes épisodes et j'attends vos commentaires qui me permettront de réaliser le meilleur livre
+    de ma carrière !</p>
+<p class="authorName">Jean FORTEROCHE</p>
+
+<?php
+while ($data = $episodes->fetch()) {
+    ?>
+<div class="episodes">
+    <h3>
+        <em>
+            <?= htmlspecialchars($data['title']) ?>
+        </em>
+    </h3>
+
+    <p>
+        <?= $extract = substr(nl2br(htmlspecialchars($data['content'])), 0, 500);
+    $space = strrpos($extract, ' ');
+    echo substr($extract, 0, $space) . '...'; ?>
+        <br />
+
+    </p>
+    <em>
+        <a href="index.php?action=post&amp;id=<?= $data['idEpisode'] ?>">Lire</a>
+    </em>
+</div>
+<?php
+}
+$episodes->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<!--lien pour faire apparaitre variables $content et $title dans le template :-->
+<?php require('template_frontend.php');
