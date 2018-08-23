e3ef10b7932cdccc76a8c5d2a5a6cc5d0f69b00e
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-23 15:44:27 +0200

    adding function to read many items

--- structure_in_MVC/Model/Repository/Model_Repository_CommentManager.php
+++ structure_in_MVC/Model/Repository/Model_Repository_CommentManager.php
@@ -10,22 +10,26 @@ require_once("Model/Interface/Model_Interface_Readable.php");
 //pour gÃ©rer les commentaires :
  abstract class CommentManager extends Manager implements Readable
  {
-     public function readItems($idEpisode)//pour lire tous les commentaires d'un épisode en fonction de son id :
+     public function readItems()
      {
-          $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT idComment, idEpisode, author, content, DATE_FORMAT(addDate, \'%d/%m/%Y à %Hh%imin%ss\') AS addDate_fr FROM comments WHERE idEpisode = ? ORDER BY addDate DESC');
-        $comments->execute(array($idEpisode));
+     }
+   
+     public function readItemsById($idEpisode)//pour lire tous les commentaires d'un épisode en fonction de son id :
+     {
+         $db = $this->dbConnect();
+         $comments = $db->prepare('SELECT idComment, idEpisode, author, content, DATE_FORMAT(addDate, \'%d/%m/%Y à %Hh%imin%ss\') AS addDate_fr FROM comments WHERE idEpisode = ? ORDER BY addDate DESC');
+         $comments->execute(array($idEpisode));
 
-        return $comments;
+         return $comments;
      }
      
      public function readById($idComment)//récupère un commentaire précis en fonction de son id :
      {
-          $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id = ?');
-        $req->execute(array($idComment));
-        $comment = $req->fetch();
+         $db = $this->dbConnect();
+         $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id = ?');
+         $req->execute(array($idComment));
+         $comment = $req->fetch();
  
-        return $comment;
+         return $comment;
      }
  }
