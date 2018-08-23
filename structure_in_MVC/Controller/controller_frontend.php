a419591e0ba39452ebc9a28b7c354637cc9fb56a
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-23 16:18:29 +0200

    adding function (detailsEpisode) to read episode and his comments

--- structure_in_MVC/Controller/controller_frontend.php
+++ structure_in_MVC/Controller/controller_frontend.php
@@ -4,6 +4,8 @@
 require_once('Model/Entity/Model_Entity_Episode.php');
 require_once("Model/Repository/Model_Repository_EpisodeManager.php");
 require_once("Model/Repository/Model_Repository_EpisodeRepo.php");
+require_once("Model/Repository/Model_Repository_CommentManager.php");
+require_once("Model/Repository/Model_Repository_CommentRepo.php");
 
 function listEpisodes()//affiche la liste des posts :
 {
@@ -15,16 +17,16 @@ function listEpisodes()//affiche la liste des posts :
     require('view/frontend/p1_listEpisodesView.php');
 }
 
-function episode()
+function detailsEpisode()
 {
     //intanciation objet $postManager :
-    $episodeManager = new \projet4\Blog\Model\EpisodeRepo();
+    $episode = new \projet4\Blog\Model\EpisodeRepo();
     //intanciation objet $commentManager :
-    //$commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+    $comment = new \projet4\Blog\Model\CommentRepo();
 
     //instanciation des objets $post et $comment pour les utiliser dans postView :
-    $episode = $episodeManager->readEpisode($_GET['id']);//récupère un épisode précis en fonction de son id :
-    //$comments = $commentManager->getComments($_GET['id']);//récupère les commentaires associés à un ID de post.
+    $detailsEpisode = $episode->readEpisode($_GET['idEpisode']);//récupère un épisode précis en fonction de son id :
+    $comments = $comment->readComments($_GET['idEpisode']);//récupère les commentaires associés à un ID de post.
 
-    require('view/frontend/episodeView.php');
+    require('view/frontend/p2_detailsEpisodeView.php');
 }
