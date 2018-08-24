a40cd52ec0b0f3280dd890f52291e72f3daa8da8
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-24 17:10:58 +0200

    adding 2 actions : addComment and contactWriter

--- structure_in_MVC/index.php
+++ structure_in_MVC/index.php
@@ -1,17 +1,31 @@
 <?php
-require('controller/frontend.php');
+require('Controller/controller_frontend.php');
 
 try { // On essaie de faire des choses
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'listEpisodes') {
             listEpisodes();
-        } elseif ($_GET['action'] == 'episode') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                episode();
+        } elseif ($_GET['action'] == 'detailsEpisode') {
+            if (isset($_GET['idEpisode']) && $_GET['idEpisode'] > 0) {
+                detailsEpisode();
             } else {
                 // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
                 throw new Exception('Aucun identifiant de billet envoyé');
             }
+        } elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['idEpisode']) && $_GET['idEpisode'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['content'])) {
+                    addComment($_GET['idEpisode'], $_POST['author'], $_POST['content']);
+                } else {
+                    // Autre exception
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            } else {
+                // Autre exception
+                throw new Exception('Aucun identifiant d\'épisode envoyé');
+            }
+        } elseif ($_GET['action'] == 'contactWriter') {
+            contactWriter();
         }
     } else {
         listEpisodes();
