d03837deb429539677d9ac93fd3ee9eb98f426d0
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-24 15:09:12 +0200

    adding function of interface creatable to create many comments on an episode in CommentManager

--- structure_in_MVC/Model/Repository/Model_Repository_CommentManager.php
+++ structure_in_MVC/Model/Repository/Model_Repository_CommentManager.php
@@ -5,11 +5,13 @@ namespace projet4\Blog\Model;
 //lien pour la class Manager dont hÃ©rite la class CommentManager :
 require_once("Model/Repository/Model_Repository_Manager.php");
 require_once("Model/Interface/Model_Interface_Readable.php");
+require_once("Model/Interface/Model_Interface_Creatable.php");
 
 
 //pour gÃ©rer les commentaires :
- abstract class CommentManager extends Manager implements Readable
+ abstract class CommentManager extends Manager implements Readable, Creatable
  {
+     /*fonctions pour interface readable----*/
      public function readItems()
      {
      }
@@ -26,10 +28,20 @@ require_once("Model/Interface/Model_Interface_Readable.php");
      public function readById($idComment)//récupère un commentaire précis en fonction de son id :
      {
          $db = $this->dbConnect();
-         $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id = ?');
+         $req = $db->prepare('SELECT idComment, author, content, DATE_FORMAT(addDate, \'%d/%m/%Y à %Hh%imin%ss\') AS addDate_fr FROM comments WHERE idComment = ?');
          $req->execute(array($idComment));
          $comment = $req->fetch();
  
          return $comment;
+     }
+
+     /*fonctions pour interface creatable----*/
+     public function createItemsByIds($idEpisode, $author, $content)
+     {
+         $db = $this->dbConnect();
+         $comments = $db->prepare('INSERT INTO comments(idEpisode, author, content, addDate) VALUES(?, ?, ?, NOW())');
+         $affectedLines = $comments->execute(array($idEpisode, $author, $content));
+  
+         return $affectedLines;
      }
  }
