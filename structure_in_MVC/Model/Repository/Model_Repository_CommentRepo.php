c5bd9da84562a71023380c86d72a63a6dda8522c
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-24 15:11:30 +0200

    adding function of interface and commentManager to create a comment

--- structure_in_MVC/Model/Repository/Model_Repository_CommentRepo.php
+++ structure_in_MVC/Model/Repository/Model_Repository_CommentRepo.php
@@ -7,9 +7,15 @@ require_once("Model/Repository/Model_Repository_CommentManager.php");
 
 class CommentRepo extends CommentManager
 {
+    public function createComments($idEpisode, $author, $content)//créer des commentaires en fonction d'un épisode :
+    {
+        $createComments = $this->createItemsByIds($idEpisode, $author, $content);
+        return $createComments;
+    }
+
     public function readComments($idEpisode)//récupère tous les commentaires d'un épisode :
     {
-        $readComments = $this->readItems($idEpisode);
+        $readComments = $this->readItemsById($idEpisode);
         return $readComments;
     }
 
