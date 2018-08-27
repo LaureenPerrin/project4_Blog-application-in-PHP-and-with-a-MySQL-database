49244ad0a904e9087ed690e3764a79e8b226f4e4
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-27 14:12:33 +0200

    delate comments in lines code

--- structure_in_MVC/Model/Repository/Model_Repository_EpisodeRepo.php
+++ structure_in_MVC/Model/Repository/Model_Repository_EpisodeRepo.php
@@ -2,20 +2,19 @@
 
 namespace projet4\Blog\Model;
 
-//lien pour la class Episode dont hérite la class EpisodeRepo :
 require_once("Model/Repository/Model_Repository_EpisodeManager.php");
 
 class EpisodeRepo extends EpisodeManager
 {
-    public function readEpisodes()//récupère tous épisodes du blog :
+    public function readEpisodes()//récupère tous les épisodes du blog :
     {
         $readEpisodes = $this->readItems();
         return $readEpisodes;
     }
 
-    public function readEpisode($Id)//récupère tous épisodes du blog :
+    public function readEpisode($idEpisode)//récupère un épisodes du blog en fonction de son id:
     {
-        $readEpisode = $this->readById($Id);
+        $readEpisode = $this->readById($idEpisode);
         return $readEpisode;
     }
 }
