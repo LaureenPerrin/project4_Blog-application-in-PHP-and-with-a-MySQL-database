b6bc236c39b8a1b466a283d0946ec6c0f061285a
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-27 11:06:50 +0200

    change name function contactWriter for getWriterContact, delate unnecessary comments in all functions, change name var  for insertComment

--- structure_in_MVC/Controller/controller_frontend.php
+++ structure_in_MVC/Controller/controller_frontend.php
@@ -9,38 +9,33 @@ require_once("Model/Repository/Model_Repository_CommentRepo.php");
 
 function listEpisodes()//affiche la liste des épisodes :
 {
-    //intanciation objet $episode :
     $episode = new \projet4\Blog\Model\EpisodeRepo();
-    //instanciation objet $episode pour l'utiliser dans p1_listEpisodesView :
+    
     $episodes = $episode->readEpisodes();//récupère tous les derniers épisodes du blog :
     
     require('view/frontend/p1_listEpisodesView.php');
 }
 
-function detailsEpisode()//affiche un épisode en détail :
+function detailsEpisode()//affiche un épisode en détail avec ses commentaires :
 {
-    //intanciation objet $episode:
     $episode = new \projet4\Blog\Model\EpisodeRepo();
-    //intanciation objet $comment :
+    
     $comment = new \projet4\Blog\Model\CommentRepo();
 
-    //instanciation des objets $detailsEpisode et $comments pour les utiliser dans listEpisodeView :
     $detailsEpisode = $episode->readEpisode($_GET['idEpisode']);//récupère un épisode précis en fonction de son id :
-    $comments = $comment->readComments($_GET['idEpisode']);//récupère les commentaires associés à un ID de post.
+    $comments = $comment->readComments($_GET['idEpisode']);//récupère les commentaires associés à un ID d'épisode :
 
     require('view/frontend/p2_detailsEpisodeView.php');
 }
 
 function addComment($idEpisode, $author, $content)//ajoute un commmmentaire à un épisode et reste affiché sur la page actuelle :
 {
-    //intanciation objet $comment :
     $comment = new \projet4\Blog\Model\CommentRepo();
     
-    //intanciation objet $affectedLines :
-    $affectedLines = $comment->createComments($idEpisode, $author, $content);//insérer un commentaire :
+    //est ce vraiment utile :
+    $insertComment = $comment->createComments($idEpisode, $author, $content);//insérer un commentaire :
 
-    //si l'objet est false alors on lève un exception :
-    if ($affectedLines === false) {
+    if ($insertComment === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
     } else {
         //sinon on redirige vers le début de cette action :
@@ -49,7 +44,7 @@ function addComment($idEpisode, $author, $content)//ajoute un commmmentaire à u
 }
 
 
-function contactWriter()
+function getWriterContact()
 {
     require('view/frontend/p3_contactView.php');
 }
