61d6007cd8aa4138b645ec948ac07c0363edf0e1
Laureen Perrin <laureencoupe@hotmail.fr>
2018-08-27 14:08:51 +0200

    delate comments in lines code

--- structure_in_MVC/Model/Repository/Model_Repository_AdminManager.php
+++ structure_in_MVC/Model/Repository/Model_Repository_AdminManager.php
@@ -1,8 +1,7 @@
 <?php
-//pour gérer l'admin :
+
 namespace projet4\Blog\Model;
 
-//lien pour la class Manager dont hÃ©rite la class AdminManager :
 require_once("Model/Repository/Model_Repository_Manager.php");
 require_once("Model/Interface/Model_Interface_Readable.php");
 
@@ -10,7 +9,7 @@ require_once("Model/Interface/Model_Interface_Readable.php");
 //pour gÃ©rer l'admin :
  abstract class AdminManager extends Manager implements Readable
  {
-     public function readItem()//rÃ©cupÃ¨re tous les derniers posts de blog :
+     public function readItem()
      {
      }
  }
