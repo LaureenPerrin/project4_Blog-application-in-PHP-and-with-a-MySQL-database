74d77860ee76940c9692396a3e13a73085e544b7
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-02 21:18:14 +0200

    change namespace and adding function readItemByGetPost

--- structure_in_MVC/Model/Interface/Model_Interface_Readable.php
+++ structure_in_MVC/Model/Interface/Model_Interface_Readable.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace projet4\Blog\Model;
+namespace projet4\Model\Interfaces;
 
 //pour lire /récupérer item :
 interface Readable
 {
     public function readItems();
+    public function readItemByGetPost($postName, $postPassword);
     public function readItemsById($idItem);
     public function readById($idItem);
 }
