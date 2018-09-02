bee8b3372246364d92e5f510320526c1b53f1338
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-02 21:28:33 +0200

    change namespace, adding the use operator and function readItemByGetPost

--- structure_in_MVC/Model/Repository/Model_Repository_AdminManager.php
+++ structure_in_MVC/Model/Repository/Model_Repository_AdminManager.php
@@ -1,15 +1,35 @@
 <?php
 
-namespace projet4\Blog\Model;
+namespace projet4\Model\Repository;
+use projet4\Model\Repository\Manager;
+use projet4\Model\Interfaces\Readable;
 
 require_once("Model/Repository/Model_Repository_Manager.php");
-require_once("Model/Interface/Model_Interface_Readable.php");
+require_once("Model/Interfaces/Model_Interface_Readable.php");
 
 
 //pour gÃ©rer l'admin :
  abstract class AdminManager extends Manager implements Readable
  {
-     public function readItem()
+     /*---fonctions readable interface-----*/
+     public function readItems()//récupère les données admin :
+     {
+     }
+
+     public function readItemByGetPost($postName, $postPassword)
+     {
+         $db = $this->dbConnect();
+         $req = $db->prepare('SELECT * FROM admin WHERE pseudo = ? AND password = ?');
+         $req->execute(array($postName, $postPassword));
+
+         return $req;
+     }
+
+     public function readItemsById($idItem)
+     {
+     }
+
+     public function readById($idItem)
      {
      }
  }
