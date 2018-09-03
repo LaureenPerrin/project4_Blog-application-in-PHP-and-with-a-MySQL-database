157b48de23a195a38bbd883989f6e1d0d50a870a
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-03 15:50:58 +0200

    delate function readItemByGetPost

--- structure_in_MVC/Model/Interface/Model_Interface_Readable.php
+++ structure_in_MVC/Model/Interface/Model_Interface_Readable.php
@@ -6,7 +6,6 @@ namespace projet4\Model\Interfaces;
 interface Readable
 {
     public function readItems();
-    public function readItemByGetPost($postName, $postPassword);
     public function readItemsById($idItem);
     public function readById($idItem);
 }
