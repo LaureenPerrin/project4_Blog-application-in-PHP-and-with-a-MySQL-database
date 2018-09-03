d7868f9e4ab0f47875456e4437871ae81d4245b7
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-03 16:15:45 +0200

    adding adminEntity variable and addinc functions for admin and backend space

--- structure_in_MVC/Controller/ControllerBackend.php
+++ structure_in_MVC/Controller/ControllerBackend.php
@@ -1,8 +1,10 @@
 <?php
 
-Use \projet4\Model\Repository\EpisodeRepo;
+use \projet4\Model\Repository\EpisodeRepo;
 use \projet4\Model\Repository\AdminRepo;
 use \projet4\Model\Repository\CommentRepo;
+use \projet4\Model\Entity\Admin;
+
 // Chargement des classes :
 require_once('Model/Entity/Model_Entity_Episode.php');
 require_once('Model/Entity/Model_Entity_Admin.php');
@@ -18,44 +20,54 @@ class ControllerBackend
     private $_episode;
     private $_comment;
     private $_admin;
+    private $_adminEntity;
 
     public function __construct()
     {
         $this->_episode = new EpisodeRepo();
         $this->_comment = new CommentRepo();
         $this->_admin = new AdminRepo();
+        $this->_adminEntity = new Admin();
     }
 
-    public function formConnectionAdmin()
+    public function formConnectionAdmin()//Affiche le formulaire de connexion à l'espace administrateur :
     {
-        
         require('view/backend/connectionAdminView.php');
-       /* 
-        if (isset($_POST) and isset($_POST['pseudo']) and isset($_POST['password'])) {
-            $routeBackend->connectionAdmin($_POST['pseudo'], $_POST['password']);
-if (!empty(htmlspecialchars($_POST['pseudo']) AND !empty($_POST['password']))) {
-            $passwordHash = password_hash($postPassword, PASSWORD_DEFAULT);
-            $connectionAdmin = $this->_admin->readAdmin($postName, $postPassword);//$postName, $postPassword
-            if ($connectionAdmin) {
-                $_SESSION['admin'] = $_POST['pseudo'];
-                header('Location: index.php?action=listEpisodesAdmin');
+    }
+
+    public function connectionAdmin()//Permet de se connecter à l'espace administrateur :
+    {
+        $admin = $this->_admin->readAdmin();
+        $dataBaseAdmin = $admin->fetch();//récupére les données de l'admin
+        if (isset($_POST) and !empty(htmlspecialchars($_POST['pseudo'])) and !empty(htmlspecialchars($_POST['password']))) {
+            $isCorrectPassword = $this->_adminEntity->PasswordVerification($_POST['password'], $dataBaseAdmin['password']);
+            if (($_POST['pseudo'] == $dataBaseAdmin['pseudo']) and $isCorrectPassword) {
+                //Vérifie si l'id de la session créée est bien celui attribué à l'admin :
+                if (session_id() == $dataBaseAdmin['idSession']) {
+                    header('Location: index.php?action=listEpisodesAdmin');
+                } else {
+                    $error = "Vous n'avez pas les droits suffisants pour accéder à ces pages.";
+                }
             } else {
-                $error = 'Identifiants incorrects';
+                $error = "Identifiants incorrects !" ;
             }
         } else {
-            $error = 'Veuillez remplir tous les champs !';
+            $error = "Veuillez remplir tous les champs s'il vous plaît !";
         }
-    }
-        if (isset($error)) {
-             echo $error;
-         } 
-        require('view/backend/connectionAdminView.php');*/
+        require('view/backend/connectionAdminView.php');
     }
 
-    /*public function listEpisodesAdmin()//affiche la liste des épisodes :
+    public function listEpisodesAdmin()//affiche la liste des épisodes :
     {
         $episodes = $this->_episode->readEpisodes();//récupère tous les derniers épisodes du blog :
-        require('view/backend/listEpisodesAdminView.php');
-    }*/
+        require('View/backend/listEpisodesAdminView.php');
+    }
 
+    public function logoutAdmin()//Permet de se déconnecter de l'espace administrateur :
+    {
+        $_SESSION = array();
+        session_destroy();
+        $error = "vous êtes maintenant déconnecté.";
+        require('view/backend/connectionAdminView.php');
+    }
 }
