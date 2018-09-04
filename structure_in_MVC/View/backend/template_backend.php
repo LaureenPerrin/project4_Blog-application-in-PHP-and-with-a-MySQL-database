1e65369dce607d04f045726297f28aeb584a9010
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-04 15:07:49 +0200

    adding script tu use cdn of TinyMCE

--- structure_in_MVC/View/backend/template_backend.php
+++ structure_in_MVC/View/backend/template_backend.php
@@ -37,6 +37,22 @@
         crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="public/css/style.css" />
 
+    <script type="text/javascript" src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector: '#myTextarea',
+            theme: 'modern',
+            height: 500,
+            plugins: "autoresize",
+            plugins: [
+                'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+                'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+                'save table contextmenu directionality emoticons template paste textcolor'
+            ],
+            content_css: 'css/content.css',
+            toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+        });
+    </script>
 
 
 </head>
