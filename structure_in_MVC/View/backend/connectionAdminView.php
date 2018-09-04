1cd58fd42b81577c64d3b5ce69e231e0e9d71b57
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-04 10:24:12 +0200

    adding management error and adding id for div of form

--- structure_in_MVC/View/backend/connectionAdminView.php
+++ structure_in_MVC/View/backend/connectionAdminView.php
@@ -3,23 +3,26 @@
 <?php ob_start(); ?>
 
 <h1 class="titleConnectAdmin">Espace administrateur</h1>
-
+<?php 
+ if (isset($error)) {
+     echo '<div class="error">' . $error . '</div>';
+ }
+     ?>
 <div class="connection">
-
 
     <form class="formConnectionAdmin" action="index.php?action=connectionAdmin" method="post">
 
-        <div class="addComment">
+        <div id="pseudoAdmin" class="addComment">
             <label for="pseudo">Identifiant :</label>
             <br />
             <input type="text" id="pseudo" name="pseudo" />
         </div>
-        <div class="addComment">
+        <div id="passwordAdmin" class="addComment">
             <label for="password">Mot de passe :</label>
             <br />
             <input type="password" id="password" name="password" />
         </div>
-        <div>
+        <div id="buttonConnectionAdmin">
             <input type="submit" value="Connexion" />
         </div>
     </form>
