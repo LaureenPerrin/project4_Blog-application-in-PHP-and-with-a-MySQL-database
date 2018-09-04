37b5e10992bbed1a6a9dbba1f9d8acd1087a8911
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-04 10:27:31 +0200

    adding listEpisodesAdmin view

---
+++ structure_in_MVC/View/backend/listEpisodesAdminView.php
@@ -0,0 +1,36 @@
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+
+<h2>Liste des épisodes</h2>
+<?php
+while ($data = $episodes->fetch()) {
+    ?>
+<div class="episodes">
+    <h3>
+        <em>
+            <?= htmlspecialchars($data['title']) ?>
+        </em>
+    </h3>
+
+    <p>
+        <?php
+        $extract = substr(nl2br(htmlspecialchars($data['content'])), 0, 500);
+    $space = strrpos($extract, ' ');
+    echo substr($extract, 0, $space) . '...'; ?>
+        <br />
+
+    </p>
+    <em>
+        <a href="index.php?action=updateEpisodeView&amp;idEpisode=<?= $data['idEpisode'] ?>">Mettre
+            à jour</a>
+    </em>
+</div>
+<?php
+}
+$episodes->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<!--lien pour faire apparaitre variables $content et $title dans le template :-->
+<?php require('template_backend.php');
