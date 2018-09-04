c580d571b7a6210b51c8f1ffb90cbf8be50927d5
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-04 15:04:04 +0200

    adding a link to the page addEpisodeView

--- structure_in_MVC/View/backend/listEpisodesAdminView.php
+++ structure_in_MVC/View/backend/listEpisodesAdminView.php
@@ -25,11 +25,18 @@ while ($data = $episodes->fetch()) {
         <a href="index.php?action=updateEpisodeView&amp;idEpisode=<?= $data['idEpisode'] ?>">Mettre
             à jour</a>
     </em>
+
+
+
 </div>
 <?php
 }
+
 $episodes->closeCursor();
 ?>
+<em>
+    <a href="index.php?action=adminFormToAddEpisode">Ajouter un épisode</a>
+</em>
 <?php $content = ob_get_clean(); ?>
 
 <!--lien pour faire apparaitre variables $content et $title dans le template :-->
