3d1016c3a08301e71efee26de67c398c9d874411
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-05 11:47:11 +0200

    adding function createEpisode to add episode in database

--- structure_in_MVC/Model/Repository/Model_Repository_EpisodeRepo.php
+++ structure_in_MVC/Model/Repository/Model_Repository_EpisodeRepo.php
@@ -3,10 +3,17 @@
 namespace projet4\Model\Repository;
 
 use projet4\Model\Repository\EpisodeManager;
+
 require_once("Model/Repository/Model_Repository_EpisodeManager.php");
 
 class EpisodeRepo extends EpisodeManager
 {
+    public function createEpisodes($title, $content)//créer des commentaires en fonction d'un épisode :
+    {
+        $createEpisodes = $this->createItemByDataPost($title, $content);
+        return $createEpisodes;
+    }
+
     public function readEpisodes()//récupère tous les épisodes du blog :
     {
         $readEpisodes = $this->readItems();
