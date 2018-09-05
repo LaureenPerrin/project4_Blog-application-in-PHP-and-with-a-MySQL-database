4c2ffc075c9760464a5e5243965c74395123cd7c
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-05 11:45:47 +0200

    adding use operator and require_once for creatable interface and adding functions to creatable interface

--- structure_in_MVC/Model/Repository/Model_Repository_EpisodeManager.php
+++ structure_in_MVC/Model/Repository/Model_Repository_EpisodeManager.php
@@ -4,13 +4,28 @@ namespace projet4\Model\Repository;
 
 use projet4\Model\Repository\Manager;
 use projet4\Model\Interfaces\Readable;
+use projet4\Model\Interfaces\Creatable;
 
 require_once("Model/Repository/Model_Repository_Manager.php");
 require_once("Model/Interfaces/Model_Interface_Readable.php");
-
+require_once("Model/Interfaces/Model_Interface_Creatable.php");
 //pour gÃ©rer les épisodes :
- abstract class EpisodeManager extends Manager implements Readable
+ abstract class EpisodeManager extends Manager implements Readable, Creatable
  {
+     /*fonctions pour interface creatable----*/
+     public function createItemsByIds($idEpisode, $author, $content)
+     {
+     }
+
+     public function createItemByDataPost($title, $content)
+     {
+         $db = $this->dbConnect();
+         $episodes = $db->prepare('INSERT INTO episodes(title, content, episodeDate) VALUES(?, ?, NOW())');
+         $affectedLines = $episodes->execute(array($title, $content));
+   
+         return $affectedLines;
+     }
+
      public function readItems()//récupère tous les épisodes :
      {
          $db = $this->dbConnect();
