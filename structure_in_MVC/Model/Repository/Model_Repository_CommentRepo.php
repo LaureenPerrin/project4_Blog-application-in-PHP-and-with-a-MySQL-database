d8cb5aa81815cc53f8c344e6212378134be0d905
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-06 20:40:21 +0200

    adding functions to delate comment

--- structure_in_MVC/Model/Repository/Model_Repository_CommentRepo.php
+++ structure_in_MVC/Model/Repository/Model_Repository_CommentRepo.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace projet4\Model\Repository;
+
 use projet4\Model\Repository\CommentManager;
+
 require_once("Model/Repository/Model_Repository_CommentManager.php");
 
 class CommentRepo extends CommentManager
@@ -22,5 +24,17 @@ class CommentRepo extends CommentManager
     {
         $readComment = $this->readById($idComment);
         return $readComment;
+    }
+
+    public function delateComment($idComment, $idEpisode)
+    {
+        $delateComment = $this->delateItemByIds($idComment, $idEpisode);
+        return $delateComment;
+    }
+
+    public function delateComments($idEpisode)
+    {
+        $delateComments = $this->delateItemsById($idEpisode);
+        return $delateComments;
     }
 }
