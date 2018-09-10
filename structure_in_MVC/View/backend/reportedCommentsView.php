5d4d39d41165e6f67332e8de61f9e2776e7921ee
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-10 11:52:07 +0200

    adding href for link line 32

--- structure_in_MVC/View/backend/reportedCommentsView.php
+++ structure_in_MVC/View/backend/reportedCommentsView.php
@@ -26,7 +26,7 @@
         if ($comment['isReported'] === '1' and $comment['isModerate'] === '1') {
             echo '<p class="moderateCommentMessage">Ce commentaire est modéré.</p>';
         } ?>
-        <a href="index.php?action=editViewComment&amp;idComment=<?= $comment['idComment'] ?>"><input
+        <a href="index.php?action=publishedComments&amp;idComment=<?= $comment['idComment'] ?>"><input
                 type="button" value="Publier" /></a>
         <a href="index.php?action=moderatedComment&amp;idComment=<?= $comment['idComment'] ?>"><input
                 type="button" value="Modérer" /></a>
