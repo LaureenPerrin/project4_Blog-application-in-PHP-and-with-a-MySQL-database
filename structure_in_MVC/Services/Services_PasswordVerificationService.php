d841cc1437d377385c5519638a6012737d5a0995
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-11 15:59:08 +0200

    adding function to verify the pseudo of admin whand he is connect to admin space

--- structure_in_MVC/Services/Services_PasswordVerificationService.php
+++ structure_in_MVC/Services/Services_PasswordVerificationService.php
@@ -2,11 +2,15 @@
 
 namespace projet4\services;
 
-//Service pour vérifier le password de l'admin lors de la connexion à l'espace administrateur :
+//Service pour vérifier le password et le pseudo de l'admin lors de la connexion à l'espace administrateur :
 class PasswordVerificationService
 {
     public static function isPasswordCorrect($passwordByPost, $passwordDataBaseAdmin)
     {
         return password_verify($passwordByPost, $passwordDataBaseAdmin);
+    }
+
+    public static function isPseudoCorrect($pseudoByPost, $pseudoDataBaseAdmin){
+        return $pseudoByPost == $pseudoDataBaseAdmin;
     }
 }
