ed6971569ad7f8c5bd751c2f7fdd6dc22c610dba
Laureen Perrin <laureencoupe@hotmail.fr>
2018-09-11 21:44:10 +0200

    adding function in interface

--- structure_in_MVC/Model/Interfaces/Model_Interface_Updatable.php
+++ structure_in_MVC/Model/Interfaces/Model_Interface_Updatable.php
@@ -8,4 +8,5 @@ interface Updatable
     public function updateItemByIds($ContentItem, $idItem);
     public function updateItemById($idItem);
     public function updateItemByDataGet($idItem);
+    public function updateItemByDataPost($titleItem, $ContentItem, $idItem);
 }
